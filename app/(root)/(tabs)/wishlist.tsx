6475d30b49f03d93dcc13440a9a4fdc958accd85
pyPaulson <agyekumpaul07@gmail.com>
2026-01-02 23:18:17 +0000

    wishlist screen updated

--- app/(root)/(tabs)/wishlist.tsx
+++ app/(root)/(tabs)/wishlist.tsx
@@ -1,35 +1,75 @@
 import RecommendationCard from "@/components/cards/RecommendationCard";
 import { useWishlist } from "@/context/WishlistContext";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import {
+  FlatList,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const WishlistScreen = () => {
   const { wishlist } = useWishlist();
 
+  const isWishlistEmpty = wishlist.length === 0;
+
   return (
-    <ScrollView className="p-4 mt-16">
-      <Text className="text-2xl font-montserrat-extraBold text-black text-center">
-        Wishlist
-      </Text>
-      {wishlist.length === 0 ? (
-        <Text className="text-center mt-10 text-gray-500">
-          No items in wishlist yet...
-        </Text>
-      ) : (
-        wishlist.map((item) => (
-          <RecommendationCard
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            title={item.title}
-            category={item.category}
-            oldPrice={item.oldPrice}
-            price={item.price}
-            rating={item.rating}
-            reviews={item.reviews}
+    <ScrollView>
+      <View className="flex-1 px-4">
+        {/* Title */}
+        <View className="items-center pt-16 mb-6">
+          <Text className="text-xl font-montserrat-extraBold text-black text-center">
+            Wishlist
+          </Text>
+        </View>
+
+        {/* Empty State */}
+        {isWishlistEmpty ? (
+          <View className="flex-1 items-center mt-20">
+            <Ionicons name="heart-outline" size={50} color="#000" />
+
+            <Text className="text-lg font-montserrat-bold text-gray-700 mt-4 mb-2">
+              Your Wishlist is Empty
+            </Text>
+
+            <Text className="text-gray-500 mb-6 text-center w-3/4">
+              Browse products and tap the heart icon to save your favorites.
+            </Text>
+
+            <TouchableOpacity
+              onPress={() => router.push("/")}
+              className="bg-black px-6 py-3 rounded-xl"
+              activeOpacity={0.8}
+            >
+              <Text className="text-white font-montserrat-bold">Shop now</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          /* Wishlist Items */
+          <FlatList
+            data={wishlist}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 40 }}
+            scrollEnabled={false}
+            renderItem={({ item }) => (
+              <RecommendationCard
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                category={item.category}
+                oldPrice={item.oldPrice}
+                price={item.price}
+                rating={item.rating}
+                reviews={item.reviews}
+              />
+            )}
           />
-        ))
-      )}
+        )}
+      </View>
     </ScrollView>
   );
 };
