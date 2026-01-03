222de978ca4668aca4f8293f6c485ef96b7f78a0
pyPaulson <agyekumpaul07@gmail.com>
2026-01-03 23:46:47 +0000

    cart screen updated

--- app/(root)/screens/[id].tsx
+++ app/(root)/screens/[id].tsx
@@ -205,6 +205,7 @@ export default function ProductDetail() {
               router.push({
                 pathname: "./productDetails/[id]" as any,
                 params: {
+                  id,
                   image,
                   price,
                   title,
